3b029f7b40da7670f0fd1c5e5477cef6a1b6d338
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-09-29 20:33:15 +0800

    Feat: Add initial UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,70 @@
+import { useState, useEffect } from "react";
+import formatDate from "./utils/formatDate";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+interface Post {
+  id: string;
+  title: string;
+  content: string;
+  publishedAt: string;
+}
+
 function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch(`${apiUrl}/posts`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjFkMDFmMzVhLWExYmEtNGFjNS05OGYwLTc0MWYzNzFkMWI5NSIsInVzZXJuYW1lIjoiYWltbmR6IiwiaWF0IjoxNzI3NTg3OTQ0LCJleHAiOjE3Mjc1OTE1NDR9.jildczaZsS0gScLxaa1X6sD306waHxYU4OSuGsPhbVs`,
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
-    <>
-      <h1 className="text-center">Hello world</h1>
-    </>
+    <div className="bg-primary-900 text-primary-100 font-montserrat h-screen">
+      <div className="max-w-4xl mx-auto">
+        <main className="mx-4">
+          <div className="mx-auto">
+            <nav className="flex justify-between items-center py-3">
+              <div>
+                <p className="font-bold text-xl text-accent-100 ">AimPoint</p>
+              </div>
+              <ul>
+                <li>
+                  <button className="bg-primary-800 text-accent-100 px-6 py-3 rounded-lg border border-solid border-primary-300 ">
+                    Login
+                  </button>
+                </li>
+              </ul>
+            </nav>
+            <ul className="grid grid-cols-3 gap-3">
+              {posts.map((post: Post) => (
+                <li key={post.id}>
+                  <div className="bg-primary-800 p-3 rounded-lg border border-solid border-primary-300">
+                    <span className="text-primary-200">
+                      {formatDate(post.publishedAt)}
+                    </span>
+                    <h1 className="text-accent-100 text-2xl font-semibold ">
+                      {post.title}
+                    </h1>
+                    <p className="text-primary-200">{post.content}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-100": "#D6D6D6",
+        "primary-200": "#828282",
+        "primary-300": "#404040",
+        "primary-800": "#131A2B",
+        "primary-900": "#0A101F",
+
+        "accent-100": "#DAD449",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
