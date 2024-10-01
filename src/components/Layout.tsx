3d778ac42e8148762d97d028fe3fbbf7cf6af73d
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-10-01 23:02:09 +0800

    Fix: Use min-h-screen for full background height

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="bg-primary-900 text-primary-100 font-montserrat h-screen">
+    <div className="bg-primary-900 text-primary-100 font-montserrat min-h-screen">
       <div className="max-w-4xl mx-auto">
         <div className="mx-3">{children}</div>
       </div>
