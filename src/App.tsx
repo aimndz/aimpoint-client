b39d422b28156abe94819d18af1271f9e5e6341b
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-10-03 10:38:54 +0800

    Feat: Remove token from localstorage when expired

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import checkTokenExpiration from "./utils/checkTokenExp";
 
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Layout from "./components/Layout";
 import Signup from "./components/Signup";
 import Post from "./components/Post";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const isTokenExpired = checkTokenExpiration();
+
+    if (isTokenExpired) {
+      localStorage.removeItem("token");
+    }
+  }, []);
+
   return (
     <Router>
       <Layout>
