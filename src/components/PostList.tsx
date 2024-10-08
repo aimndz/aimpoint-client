ab42bc2fabedc662a5d23040c58e2d96be0050dd
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-10-09 00:46:42 +0800

    Feat: Truncate content on view

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -19,26 +19,38 @@ type PostListProps = {
 const PostList = ({ filteredPosts }: PostListProps) => {
   return (
     <>
-      {filteredPosts.map((post) => (
-        <li key={post.id}>
-          <Link to={`/posts/${post.id}`}>
-            <div className="bg-primary-800 p-3 rounded-lg border border-solid border-primary-300">
-              <span className="text-primary-200 text-sm">
-                {post.publishedAt
-                  ? formatDate(post.publishedAt)
-                  : "Unpublished"}
-              </span>
-              <h1 className="text-accent-100 text-2xl font-semibold ">
-                {post.title}
-              </h1>
-              <div className="text-primary-200">{parse(post.content)}</div>
-              <p className="text-primary-200 mt-3 text-sm">
-                {post._count.Comment} comments
-              </p>
-            </div>
-          </Link>
-        </li>
-      ))}
+      {filteredPosts.map((post) => {
+        // Split the content into words and get the first 10
+        const contentWords = post.content.split(" ").slice(0, 10).join(" ");
+        // Append ellipsis if the content has more than 10 words
+        const truncatedContent =
+          post.content.split(" ").length > 10
+            ? `${contentWords}...`
+            : contentWords;
+
+        return (
+          <li key={post.id}>
+            <Link to={`/posts/${post.id}`}>
+              <div className="bg-primary-800 p-3 rounded-lg border border-solid border-primary-300">
+                <span className="text-primary-200 text-sm">
+                  {post.publishedAt
+                    ? formatDate(post.publishedAt)
+                    : "Unpublished"}
+                </span>
+                <h1 className="text-accent-100 text-2xl font-semibold ">
+                  {post.title}
+                </h1>
+                <div className="text-primary-200">
+                  {parse(truncatedContent)}
+                </div>
+                <p className="text-primary-200 mt-3 text-sm">
+                  {post._count.Comment} comments
+                </p>
+              </div>
+            </Link>
+          </li>
+        );
+      })}
     </>
   );
 };
